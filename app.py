c00fdc4cd417fd50d4c25a2dc7375e1d87fa28f1
Gabriel Causa <gabrielcausa@gmail.com>
2022-07-12 21:21:48 -0300

    Front funcional terminado, primer MVP

--- app.py
+++ app.py
@@ -23,8 +23,8 @@ if len(query_params)>0:
     print (batch)
 
 with st.sidebar:
-    selected = option_menu("", ['Problema','Solución', 'Technical approach', 'Limitaciones'],
-        icons=['patch-question', 'activity','tools','tools'], menu_icon="cast", default_index=0,
+    selected = option_menu("", ['Problema','Solución', 'Aspectos técnicos', 'Próximos pasos...'],
+        icons=['patch-question', 'activity','tools','signpost'], menu_icon="cast", default_index=0,
         styles={
         "container": {"padding": "5!important", "background-color": "#fafafa"},
         "icon": {"color": "green", "font-size": "25px"},
@@ -32,29 +32,30 @@ with st.sidebar:
         "nav-link-selected": {"background-color": "#02ab21"},
     })
 
-if selected == "Limitaciones":
+if selected == "Próximos pasos...":
     st.image("img/logo.png")
     st.markdown(f"{batch}")
+    
+    st.header("Próximos pasos...")
     st.markdown('''
-
-    # Limitaciones
-
-    - Dependencia de pronósticos para variables regresoras.
-    - Un modelo por lote, no regional.
-    - Agnóstico de la especie de pastura.
-
+    - Explorar mejor la influencia de variables exógenas.
+    - Mejorar la escalabilidad del modelo.
+    - Incorporar información sobre las especies de pasturas.    
+    - ...
     ''' )
+    # Antes...
+    #  - Dependencia de pronósticos para variables regresoras.
+    #  - Un modelo por lote, no regional.
+    #  - Agnóstico de la especie de pastura.
+
+
+
 
 else:
     if selected == "Problema":
         st.image("img/logo.png")
         st.markdown('''
-
         # Is the grass greener on the other side (of winter)?
-
-        Cattle farmers need to know how winter/summer pastures are going to behave in order to make smarter environmental and economics decisions.
-        Low temperatures/drought are main controls of pasture growth.
-
         ''' )
 
         col1, col2, col3 = st.columns(3)
@@ -62,30 +63,89 @@ else:
         col2.image('img/img3.JPG')
         col3.image('img/img2.JPG')
     else:
-        if selected == "Technical approach":
+        if selected == "Aspectos técnicos":
+            
             st.image("img/logo.png")
-
-            col1, col2, col3, col4 = st.columns(4)
-            col1.metric('Baseline (año pasado)','0.3','Comentario 1')
-            col2.metric('Random Forest','0.28','Comentario 2')
-            col3.metric('SARIMAX','0.27','Comentario 3')
-            col4.metric('Prophet','0.25','Comentario 4')
-
-
             st.markdown('''
-            # Technical approach
+                        # Productividad de pasturas
+                        ''')
 
-            - Adquisición de datos de dos APIs distintas.
-            - Limpieza de datos:
-                - Interpolar datos vacíos y extremos.
-                - Agregar datos a 8 días según cada variable.
-            - Entrenamiento en máquina local (modelos ligeros)
-
-            ''')
+            col1, col2, col3, col4 = st.columns([4,4,4,4])
+            
+            with col1:
+                st.markdown('''
+                            ### Baseline
+                            ### 0,38''')
+                #st.header('Baseline')
+                #st.metric('','0.38')
+            with col2:
+                st.markdown('''
+                            ### XGBoost
+                            ### 0,30''')
+                #st.header('XGBoost')
+                #st.metric('','0.30')  
+            with col3: 
+                st.markdown('''
+                        ### Sarimax
+                        ### 0,26''')
+                #st.header('Sarimax')
+                #st.metric('','0.26')  
+            with col4:
+                st.markdown('''
+                        ### Prophet
+                        ### 0,23''')
+                #st.header('Prophet')
+                #st.metric('','0.23')
+            
+            col5, col6 = st.columns([3,15])
+            
+            with col5:
+                model_graph = st.radio('', ('Baseline', 'XGBoost', 'Sarimax', 'Prophet'))
+            with col6:
+                if model_graph == 'Baseline':
+                    st.image('img/baseline.png', caption=None, width=None, 
+                            use_column_width=None, clamp=False, channels="RGB", output_format="auto")
+                elif model_graph == 'XGBoost':
+                    st.image('img/ML_Approach.png', caption=None, width=None, 
+                            use_column_width=None, clamp=False, channels="RGB", output_format="auto")
+                elif model_graph == 'Sarimax':
+                    st.image('img/sarimax.png', caption=None, width=None, 
+                            use_column_width=None, clamp=False, channels="RGB", output_format="auto")
+                elif model_graph == 'Prophet':
+                    st.image('img/Prophet.png', caption=None, width=None, 
+                            use_column_width=None, clamp=False, channels="RGB", output_format="auto") 
+                else:
+                    pass
+            
+            st.markdown("#### Acumulado:")   
+            
+            col7, col8, col9 = st.columns([5,5,5])
+         
+            with col7:
+                    st.markdown('''
+                                ### Baseline
+                                ### 0,22''')
+            with col8:
+                    st.markdown('''
+                                ### Random Forest
+                                ### 0,11''')
+            with col9:
+                    st.markdown('''
+                                ### XGBoost
+                                ### 0,08''')
+                    
+            st.markdown('''
+                        ##### Horizonte temporal: tres meses.
+                        ##### Mean Absolute Percentage Error (MAPE).
+                        ''')
+  
         else:
             if selected == "Solución":
 
 
+                st.markdown('''
+                # Plataforma SaaS
+                ''' )
 
                 m = folium.Map(location=[ -35, -57], zoom_start=5)
 
